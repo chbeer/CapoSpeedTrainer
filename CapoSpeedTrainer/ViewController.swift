3cb2457aa9a9f9e29e41bd85575007a4c34205d6
Christian Beer <christian.beer@chbeer.de>
2017-02-21 13:48:11 +0100

    Initial Commit

---
+++ CapoSpeedTrainer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CapoSpeedTrainer
+//
+//  Created by Christian Beer on 21.02.17.
+//  Copyright © 2017 Christian Beer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
